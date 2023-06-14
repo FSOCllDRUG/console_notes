8d28529cb2bcb3da0f34e788242d1ee4e4e9f58e
Vlad <vlad.a.borshch@gmail.com>
2023-06-14 04:02:15 +0300

    Added ASCII picture at the start.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from notes_interaction import *
 from menu import main_menu
 
 Create()
+with open('ascii-art.txt', 'r') as file:
+    data = file.read()
+print(data)
 # table.field_names = lines[0]
 main_menu()
 # os.system('python menu.py')
